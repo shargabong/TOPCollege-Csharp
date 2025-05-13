3957490b5b41c6e10c74ab91028eee2db078c625
zxc <shargabong.exe@gmail.com>
2025-05-13 15:41:37 +0300

    +OOP

---
+++ OOP(1)/OOP1.cs
@@ -0,0 +1,41 @@
+﻿class Dog //public/internal
+{
+    class DogData // Движок содержится в машине (Композиция)
+    {   // public/internal/protected/private
+        
+    }
+
+    public int Paws { get; set; } // было полем стало свойством
+
+    public int GetPaws
+    {
+        get => Paws;
+        set {  
+            if (value > 0)
+                Paws = value; 
+        }
+    }
+
+    public string Tail { private get; set; } = "Black";
+
+    public void Lie()
+    {
+
+    }
+
+    public Dog() : this(4, "Black" ){ }
+    public Dog(int paws)
+    {
+        Paws = paws;
+    }
+    public Dog(int paws, string tail)
+    {
+        Paws = paws;
+        Tail = tail;
+    }
+
+    class Puppy : Dog
+    {
+        public Puppy() : base(4) { }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿//namespace MySpace1
+//{
+//    class Person { } // класс - это шаблон
+//}
+
+//namespace MySpace2
+//{
+//    namespace MS2
+//    {
+//        class Person { }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        MySpace1.Person person = new MySpace1.Person();
+//        MySpace2.MS2.Person person2 = new MySpace2.MS2.Person();
+//    }
+//}
+
+using OOP2.Models;
+using OOP2.Service;
+
+using cm = OOP2.Models;
+using cs = OOP2.Service;
+
+class Program
+{
+    static void Main()
+    {
+        // OOP2.Models.Dog dog;
+        cm.Dog dog1;
+        cs.Dog dog2;
+    }
+}
